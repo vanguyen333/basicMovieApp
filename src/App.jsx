974a163da1b731de3b94588a2bf51bfdf8fc7e67
vanguyen333 <vanguyen333@yahoo.com>
2023-07-31 16:26:50 -0400

    useEffect, alert every change to counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
-// const Person = (props) => {
-//   return (
-//     <>
-//       <h1>Name: {props.name}</h1>
-//       <h2>Last Name: {props.lastName}</h2>
-//       <h2>Age: {props.age}</h2>
-//     </>
-//   );
-// };
-
 const App = () => {
   const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    alert("You've changed the counter to " + counter);
+  }, [counter]);
 
   return (
     <div>
-      {/* <Person name={"Na"} lastName="Pham" age="30" />
-      <Person name="Joseph" age="50" lastName="Nguyen" /> */}
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
         -
       </button>
